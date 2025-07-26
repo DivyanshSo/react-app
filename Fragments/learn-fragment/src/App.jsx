3f2ca6ab590a943e9a5c7c3b600b8306c1dd1b4e
Divyansh So <sonidivyansh005@gmail.com>
2025-07-26 20:25:24 +0530

    items added

--- Fragments/learn-fragment/src/App.jsx
+++ Fragments/learn-fragment/src/App.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import FoodItems from "./components/FoodItems";
+import ErrorMessage from "./components/ErrorMessage";
 
 
 function App() {
 
-  let foodItems = ['Dal', 'Green Vegetable', 'Roti', 'Salad', 'Milk','Eggs'];
+   let foodItems = ['Sabzi', 'Green Vegetable', 'Roti', 'Salad', 'Milk','Eggs'];
+  // let foodItems = [];
+  
+  /*Conditional rendering:- ...
+  logical(emptyMessage), ternary operators... 
+  let emptyMessage =  ?  : null  */
 
+  
+  /* usage of if-else statement
+  if(foodItems.length === 0) {
+    return <h3>I am still hungry.</h3>
+  }
+    */
   return (
     <React.Fragment> 
-      <h1>List of Healthy Foods</h1>
-      <ul className="list-group">
-        {foodItems.map((item) => (
-            <li key={item} className="list-group-item">{item}</li>
-          ))}
-      </ul>
+      <h1 className="food-heading">List of Healthy Foods</h1>
+
+      <ErrorMessage items = {foodItems}></ErrorMessage>   
+      <FoodItems items = {foodItems}></FoodItems>
     </React.Fragment>
   );
 }
---
+++ Fragments/learn-fragment/src/components/Item.jsx
@@ -0,0 +1,8 @@
+
+const Item = ({foodItem}) => {
+    return <li className="list-group-item kg-item" ><span className="kg-span">{foodItem}</span></li>
+        
+
+};
+
+export default Item;
