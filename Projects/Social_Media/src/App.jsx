6188dc820a66577d10bab6094fe2dd3d8ca0e852
Divyansh So <sonidivyansh005@gmail.com>
2025-08-23 00:58:00 +0530

    social modia

---
+++ Projects/Social_Media/src/App.jsx
@@ -0,0 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Footer/>
+      
+    </>
+  )
+}
+
+export default App;
